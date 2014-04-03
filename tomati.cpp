220ad9b6fb6d48e0d49017b3a8ffd93861e85b5b
arnisritins <arnis_ritins@inbox.lv>
2014-04-03 18:48:34 +0300

    Update tomati.cpp

--- tomati.cpp
+++ tomati.cpp
@@ -3,24 +3,20 @@ using namespace std;
 
 int main(){
 
-	int n, s, d, less, more, count = 1;
+	int n, s, d, min, count = 1;
 
 	ifstream in("tomati.dat");
 	in >> n >> s >> d;
 	in.close();
 
-	if(s-1 > n-s){
-		less = n-s;
-		more = s-1;
-	} else {
-		less = s-1;
-		more = n-s;
-	}
+	min = (n-s < s-1)
+		? n-s
+		: s-1;
 
 	if(n > 1)
-		count += (less >= d)
+		count += (min >= d)
 			? d * 2
-			: d + less;
+			: d + min;
 
 	ofstream out("tomati.rez");
 	out << count;
